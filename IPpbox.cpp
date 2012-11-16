dc9c0a2cf8fe060962259ea8da2dec15c2768cc1
isxxguo <isxxguo@pptv.com>
2012-11-16 10:12:14 +0000

    /ppbox/branches/1.2.0 /src/ppbox 调试单文件播放
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@5588 b3a1389e-2159-5c43-9038-463bf877d0d9

--- IPpbox.cpp
+++ IPpbox.cpp
@@ -16,6 +16,7 @@ using namespace ppbox::error;
 #endif
 using namespace ppbox::common;
 
+#include <ppbox/data/DataModule.h>
 #include <ppbox/demux/DemuxModule.h>
 #include <ppbox/merge/MergeModule.h>
 
@@ -110,6 +111,7 @@ namespace ppbox
 #ifndef PPBOX_DISABLE_LIVE
             util::daemon::use_module<ppbox::live::LiveModule>(*this);
 #endif
+            util::daemon::use_module<ppbox::data::DataModule>(*this);
             util::daemon::use_module<ppbox::demux::DemuxModule>(*this);
             util::daemon::use_module<ppbox::merge::MergeModule>(*this);
 #ifndef PPBOX_DISABLE_MUX
